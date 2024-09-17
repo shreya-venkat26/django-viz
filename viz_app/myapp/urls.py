937ce8e0ff6152fad8cbfba2d80899969526c24c
Shreya Venkat <shreyavenkat26@gmail.com>
2024-09-17 10:11:53 -0400

    Parentheses error resolved

--- viz_app/myapp/urls.py
+++ viz_app/myapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, "home")
+    path("", views.home, "home"),
     path('upload/', views.upload_document, name='upload'),
-    path('conversations/', views.conversation_list, name='convo_list'),
+    path('conversations/', views.conversation_list, name='convo_list')
 ]
